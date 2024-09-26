287bb7e25c0da34986c5a0c6401fb945391e46b0
IgnacioPena13 <ignacio.penasepulveda@outlook.com>
2024-09-26 11:37:37 +0200

    preliminar design test :)

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello World!");
+
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=9"
+    );
+    const data = await response.json();
+    displayData(data);
+  } catch {
+    console.error(error);
+  }
+}
+async function displayData(data) {
+  console.log(data);
+}
+
+fetchData();
