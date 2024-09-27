bc653da5655cde9a6d14dab41a5f26b21360e271
IgnacioPena13 <ignacio.penasepulveda@outlook.com>
2024-09-27 14:56:28 +0200

    Css almost finished and start JS

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 "use strict";
+window.addEventListener("load", () => {
+  retriveCategories();
+});
 
-async function fetchData() {
+const form = document.querySelector("form");
+const category = document.getElementById("select-category");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("start the game");
+});
+
+async function retriveCategories() {
+  const response = await fetch(`https://opentdb.com/api_category.php`);
+  const data = await response.json();
+  const { trivia_categories } = data;
+  console.log(trivia_categories);
+  trivia_categories &&
+    trivia_categories.map((cat) => {
+      const option = document.createElement("option");
+      option.value = cat.id;
+      option.textContent = cat.name;
+      category.appendChild(option);
+    });
+}
+
+async function fetchData(category) {
   try {
     const response = await fetch(
-      "https://opentdb.com/api.php?amount=10&category=9"
+      `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=medium&type=multiple`
     );
     const data = await response.json();
-    displayData(data);
-  } catch {
-    console.error(error);
+    console.log("data", data);
+  } catch (error) {
+    console.error("Error: ", error);
   }
 }
+
 async function displayData(data) {
-  console.log(data);
+  console.log("Data: ", data);
 }
 
-fetchData();
+function findCategory(array, userInput) {
+  const results = array.results;
+  const cats = results.map((cat) => cat.category);
+  const selectedCategory = cats.find((category) => category === userInput);
+
+  if (!selectedCategory || selectedCategory === undefined) {
+    alert("Category not found");
+  }
+
+  console.log(selectedCategory);
+
+  return selectedCategory;
+}
